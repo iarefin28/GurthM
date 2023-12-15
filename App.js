ca3377c28868a94843362d673849ab4b47396939
Ishan Arefin <ashiqul.arefin@stonybrook.edu>
2023-12-14 21:20:22 -0500

    Added some fonts async and getting started on the information gathering screen

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
@@ -8,20 +9,36 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 import SignInScreen from './src/screens/SignInScreen';
 import InformationEntryScreen from './src/screens/InformationEntryScreen';
+import * as Font from 'expo-font';
 
 export default function App() {
   const Stack = createNativeStackNavigator()
+  const [fontsLoaded, setFontsLoaded] = useState(false);
 
-  return (
-    <NavigationContainer>
+  const loadFontsAsync = async () => {
+    await Font.loadAsync({
+      'RethinkSans': require('./assets/fonts/RethinkSans.ttf'),
+      'RethinkSans-Bold': require('./assets/fonts/RethinkSans-Bold.ttf')
+    });
+    setFontsLoaded(true);
+  };
+
+  useEffect(() => {
+    loadFontsAsync();
+  }, []);
+
+  if (fontsLoaded) {
+    return (
+      <NavigationContainer>
         <View style={styles.root}>
           <Stack.Navigator initialRouteName="SignUpScreen">
             <Stack.Screen name="SignInS" component={SignInScreen} />
-            <Stack.Screen name="InformationEntryScreen" component={InformationEntryScreen}/>
+            <Stack.Screen name="InformationEntryScreen" component={InformationEntryScreen} />
           </Stack.Navigator>
         </View>
-    </NavigationContainer>
-  )
+      </NavigationContainer>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
--- src/screens/InformationEntryScreen.js
+++ src/screens/InformationEntryScreen.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { StyleSheet, Text, SafeAreaView} from 'react-native';
+import { StyleSheet, Text, SafeAreaView, View, TextInput } from 'react-native';
 
-import {useFocusEffect, useNavigation} from '@react-navigation/native'
+import { useFocusEffect, useNavigation } from '@react-navigation/native'
 import { LinearGradient } from "expo-linear-gradient";
 
 const InformationEntryScreen = () => {
     const navigation = useNavigation()
 
     useFocusEffect(() => {
-        navigation.setOptions({ 
+        navigation.setOptions({
             headerTransparent: true,
             headerTintColor: 'white',
             headerTitle: '',
@@ -23,7 +23,16 @@ const InformationEntryScreen = () => {
             end={{ x: 1, y: 1 }}
         >
             <SafeAreaView style={styles.root}>
-                <Text style={styles.header}>What's your name?</Text>
+                <View style={styles.mainContainer}>
+                    <Text style={styles.prompt}>What's your name?</Text>
+                    <Text style={styles.purpose}>Add your name so we know what to call you.</Text>
+                    <View style={styles.inputContainer}>
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Full Name"
+                        />
+                    </View>
+                </View>
             </SafeAreaView>
         </LinearGradient>
     )
@@ -31,13 +40,39 @@ const InformationEntryScreen = () => {
 
 const styles = StyleSheet.create({
     root: {
-        flex: 1,
-        flexDirection: 'column',
+        flexDirection: 'row',
+        justifyContent: 'center'
     },
-    header:{
+    mainContainer: {
+        width: '90%',
+        flexDirection: 'column',
+        marginTop: 5
+
+    },
+    prompt: {
         color: "white",
-        fontSize: '30',
-        fontFamily: 'Arial'
+        fontFamily: 'RethinkSans-Bold',
+        fontSize: 28,
+        marginBottom: 5
+    },
+    purpose: {
+        color: "white",
+        fontFamily: "RethinkSans",
+        fontSize: 14,
+        marginBottom: 20
+    },
+    inputContainer:{
+        backgroundColor: "black",
+        opacity: 0.3
+
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: "white",
+        borderRadius: 5,
+        height: 60,
+        color: "white",
+        fontFamily: "RethinkSans"
     }
 })
 
